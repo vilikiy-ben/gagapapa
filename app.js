801f31870d012e21dc878ab6ef387ed25cf48547
vilikiy-ben <hype.neiro@gmail.com>
2025-05-16 15:00:53 +0300

    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // SubsViewer - мини-приложение для Telegram
 // Версия приложения
-const APP_VERSION = "v1.0.5";
+const APP_VERSION = "v1.0.1";
 
 // Инициализация Telegram Mini App
 let tg = window.Telegram?.WebApp;
